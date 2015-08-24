465bf06f8b98510e145f1f4cead870739cd3b396
Bruno Tripoloni <bruno.tripoloni@gmail.com>
2015-08-23 21:24:36 -0300

    cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,20 @@
 class ApplicationController < ActionController::Base
-	before_filter :verify_installation
+	before_filter :startup
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def verify_installation
-  	@blog = Blog.first
-  	unless @blog
-  		redirect_to new_blog_path
-  	end
+  helper_method :blog
+
+  def blog
+    Rails.cache.read('blog')
+  end
+
+  def startup
+    cache = Rails.cache
+    unless cache.read('blog')
+      cache.write('blog', Blog.first)
+    end
   end
 end
